1b5283a4d9a0b924af54e740506f5c3e185494d5
vagrant <vagrant@vagrant.vm>
2018-02-10 00:32:32 +0000

    Refined scoring algorithm and also fixed bug for flights which return no results from the db

--- server.py
+++ server.py
@@ -29,34 +29,35 @@ def index():
 
 @app.route('/search')
 def search_flights():
-    
+   
     # Get input from form
     try:
         # get the origin and destination airports, split them into airport code and description
         origin, origin_description = request.args.get("origin").split(', ')
         destination, destination_description = request.args.get("destination").split(', ')
-    
-        # make sure a valid date was entered
-        date = request.args.get("date")
-        if date == '':
-            flash("Please enter a valid date")
-            return render_template("home.html")
-
-        # use the user's input to search the Google Flight API for results
-        # in addition, also query the database to get the correcsponding FlightScore
-        results = get_flight_results(origin, destination, date)
-
-        # extract just the time of the flight arrival and departure
-        results = update_results_for_display(results)
-
-
-        return render_template("results.html",
-                            results=results,
-                            origin=origin_description,
-                            destination=destination_description)
     except:
         flash("Please enter a valid airport")
+        return render_template("home.html")  
+
+    # make sure a valid date was entered
+    date = request.args.get("date")
+    if date == '':
+        flash("Please enter a valid date")
         return render_template("home.html")
+
+    # use the user's input to search the Google Flight API for results
+    # in addition, also query the database to get the correcsponding FlightScore
+    results = get_flight_results(origin, destination, date)
+
+    # extract just the time of the flight arrival and departure
+    results = update_results_for_display(results)
+
+
+    return render_template("results.html",
+                        results=results,
+                        origin=origin_description,
+                        destination=destination_description)
+   
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
